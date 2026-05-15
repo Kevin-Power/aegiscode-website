1cb09bc741d7cb8f7544c767c4ccdc05303746d5
Kevin-Power <Kevin-Power@users.noreply.github.com>
2026-05-15 17:50:16 +0800

    feat: 308 redirect /external-risk to /surface

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/external-risk",
+        destination: "/surface",
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default withSentryConfig(nextConfig, {
